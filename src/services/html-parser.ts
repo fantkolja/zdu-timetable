0468189d673cb648c5ae11c53adcac6ef09f5453
Mykola.Fant <fantkolja@gmail.com>
2018-11-14 22:08:07 +0200

    update(HTMLParser): get working day date

--- src/services/html-parser.ts
+++ src/services/html-parser.ts
@@ -1,14 +1,30 @@
 import * as cheerio from 'cheerio';
 
 class HtmlParser {
+  // takes a date string of the format '14.11.2018'
+  private static generateDate(inputDate: string): Date {
+    const dateArray = inputDate.trim().split('.');
+    const dateMonth = dateArray.splice(0, 2);
+    const normalizedDateString = [...dateMonth.reverse(), ...dateArray].join('.');
+    return new Date(normalizedDateString);
+  }
+
   constructor(private cheerio: CheerioAPI) {
   }
 
   public getLessons(html: string): void {
     debugger;
     const $ = this.cheerio.load(html);
-    const workingDays = $('table');
-
+    const workingDaysEls = $('.container>.row>.col-md-6');
+    const workingDays = [];
+    workingDaysEls.each((i, el) => {
+      const dateString = $(el).find('h4').contents()[0].data;
+      const date = HtmlParser.generateDate(dateString);
+      const workingDay = {
+        date,
+      };
+      workingDays[i] = workingDay;
+    });
     console.log(workingDays);
   }
 }
